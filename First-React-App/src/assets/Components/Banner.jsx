fee41e6f72d8d282e47bb87dd816ea02236c9ef6
soyalkhan <soyalkhan76@gmail.com>
2024-02-21 18:50:02 +0530

    change on project

--- First-React-App/src/assets/Components/Banner.jsx
+++ First-React-App/src/assets/Components/Banner.jsx
@@ -6,10 +6,10 @@ function Myhead() {
       <>
         <div className="MainBanner w-100 grid grid-cols-2 fl" >
             <div className="innerbanner1  p-20">
-            <h1 className='font-extrabold text-4xl text-red-700'>
+            <h1 className='font-arial font-extrabold text-4xl text-red-700'>
                 GET 100% ASSURED GROW WITH TECH DELTA
             </h1>
-            <p className='text-red-800'>Edge to edge support will be provided 100% Moenyback Guarantee
+            <p className='text-red-800 font-sans'>Edge to edge support will be provided 100% Moenyback Guarantee
                  .</p>
                  <button className='bg-blue-600 text-white rounded-md p-2 mt-4'>Enroll Now!</button>
             </div>
