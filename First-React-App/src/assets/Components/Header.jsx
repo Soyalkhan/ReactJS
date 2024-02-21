73cc4ac1e179cb9096cdc96d3586cce4345ce5f8
soyalkhan <soyalkhan76@gmail.com>
2024-02-21 14:54:36 +0530

    New-web update

--- First-React-App/src/assets/Components/Header.jsx
+++ First-React-App/src/assets/Components/Header.jsx
@@ -5,11 +5,11 @@ import '../Components/Header.css'
 function Myhead() {
   return (
     <>
-      <div className="headMain bg-[#2D3250] h-12 w-100">
+      <div className="headMain bg-[#000000] h-12 w-100 flex justify-around space-x-0 > * + *">
         <div className="logo font-semibold cursor-pointer">
-        <h1 className="text-orange-500 p-2 font-semiboldn text-lg ml-20">R•T Clothing</h1>
+        <h1 className="text-orange-500 p-2 font-semiboldn text-lg font-extrabold">Tech Delta</h1>
         </div>
-        <div className="menubar text-white p-2 text-lg content-center">
+        <div className="menubar text-white p-2 text-sm flex gap-6">
             <p className='hover:text-slate-400 cursor-pointer'>Category</p>
             <p className='hover:text-slate-400 cursor-pointer'>Collection</p>
             <p className='hover:text-slate-400 cursor-pointer'>Contact</p>
