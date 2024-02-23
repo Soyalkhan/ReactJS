d71c55b834a75a89d49176dccb2c45b74af8cea6
soyalkhan <soyalkhan76@gmail.com>
2024-02-23 23:27:25 +0530

    Change

--- UseState_HOOK/src/App.jsx
+++ UseState_HOOK/src/App.jsx
@@ -1,22 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [Number,setNumber] = useState(0)
 
+function App() {
+
+  let student = {
+    Name: "soyal",
+    Age: 27,
+    Phone: "7535964612"
+  }
+
+  const [Number,setNumber] = useState(0);
+  const [Name,setName] = useState("User Name")
+
+  const Increment = ()=>{
+    if(Number < 15){
+      setNumber(Number + 1);
+    }
+  }
+  
+  const Decrement = () =>{
+    if(Number > 0){
+      setNumber(Number - 1);
+    }
+  }
   return (
     <>
-    <h1>note by useState it will populate the output to every variable defined</h1>
+    <p>Note by useState it will populate the output to every variable as defined</p>
+   <h1>This is the initial number : {Number} </h1>
    <h1>This is the initial number : {Number} </h1>
         
-        <button onClick={()=> setNumber(Number+1)}>Update Number</button>
-        <h1>This is the initial number : {Number} </h1>
-        <h1>This is the initial number : {Number} </h1>
-        <h1>This is the initial number : {Number} </h1>
-        <h1>This is the initial number : {Number} </h1>
-        <h1>This is the initial number : {Number} </h1>
-        <h1>This is the initial number : {Number} </h1>
-        <h1>This is the initial number : {Number} </h1>
+        <button onClick={Increment}>Inrement</button><button onClick={Decrement}>Decrement</button>
+    
+       <p>Student Name is : {student.Name}</p> 
+       <p>Student age : {student.Age}</p>
+       <p>Student Phone : {student.Phone}</p>
     </>
   )
 }
