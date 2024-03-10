dbe81a432fb76b66420f9a7dcf901149bd6257e0
soyalkhan <soyalkhan76@gmail.com>
2024-03-10 23:13:53 +0530

    Contact-and-category page done

--- First-React-App/src/assets/Components/AboutUS/About.jsx
+++ First-React-App/src/assets/Components/AboutUS/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import about from '../images/aboutus.jpg';
 
 function About() {
   return (
-    <div>About</div>
+    <>
+    <h1 className='text-red-800 text-7xl font-bold	text-center p-5'>OUR STORY</h1>
+    <div className='main columns-2 p-7'>
+    <img src={about} className='w-4/5 h-80 p-3 m-5' alt="" />
+    <h2 className='text-3xl font-serif font-bold text-blue-500 '>We found somthing and build for you!</h2>
+    <p className='font-serif'>Over the course of several months we brought together a team of like-minded people and set out to develop a range of phone accessories both stylish and protective. Each product is curated mindfully to make the customer feel
+    Over the course of several months we brought together a team of like-minded people and set out to develop a range of phone accessories both stylish and protective. Each product is curated mindfully to make the customer feel
+    Over the course of several months we brought together a team of like-minded people and set out to develop a range of phone accessories both stylish and protective. Each product is curated mindfully to make the customer feel
+    </p>
+    
+    </div>
+    </>
   )
 }
 
--- First-React-App/src/assets/Components/Category/Category.jsx
+++ First-React-App/src/assets/Components/Category/Category.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import men from '../images/men.jpeg'
+import women from '../images/womens.jpeg'
 
 function Category() {
   return (
-    <div>Category</div>
+    <>
+    <div className='flex justify-center gap-4	 p-10 bg-gradient-to-t from-gray-800 via-gray-900/20"'>
+      <h1 className=' mt-3 text-8xl font-bold text-red-700 rotate-90 cursor-pointer '>Men's</h1>
+      <img className='w-1/4	object-cover cursor-pointer' src={men} alt="mens"/>
+      <h1 className=' mt-3 text-7xl font-bold text-red-700 rotate-90 cursor-pointer'>Women's</h1>
+      <img className='w-1/4	object-cover cursor-pointer' src={women} alt="womens" />
+    </div>
+    </>
   )
 }
 
