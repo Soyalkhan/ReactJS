12888757bb66ab7c57cc64108bc154ab12c1b342
soyalkhan <soyalkhan76@gmail.com>
2024-03-17 23:23:35 +0530

    cg

--- ExpenceCalculator/src/App.jsx
+++ ExpenceCalculator/src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { CiTrash } from "react-icons/ci";
+import { CiEdit } from "react-icons/ci";
 import { PieChart } from "react-minimal-pie-chart";
 import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+
+  function handleClick(){
+    inputRef.current.disable
+    inputRef.current.focus();
+  }
   return (
     <>
       <div className="heading">
@@ -13,46 +20,63 @@ function App() {
       </div>
       <div className="ms2 grid grid-cols-1 sm:grid-cols-2 bg-[#AC7D88] mb-10">
         <div className="left sm:border-solid sm:border-r-2 sm:border-[#643843]">
-          <div className="head p-5">
-            <h1 className="budget text-4xl font-bold text-[#FDF0D1]">
+          <div className="heading p-5 flex ">
+            <h3 className="budget text-2xl font-bold text-[#FDF0D1]">
               Income :{" "}
-              <span className="text-red-900 font-bold text-3xl">32000</span>
-              <hr />
-              <br />
-              <div className="add">
-                <input
-                  type="text"
-                  placeholder="Amount here."
-                  className="w-full sm:w-3/5 p-4 bg-[#FDF0D1] text-[#AC7D88] text-2xl placeholder-gray-900 rounded-lg"
-                />
-              </div>
-              <div className="task  text-black">
-                <select
-                  name=""
-                  id=""
-                  className="rounded-lg w-full sm:w-3/5 mt-5 text-2xl p-4"
-                >
-                  <option value="Fee">College Fee</option>
-                  <option value="Fee">Personal</option>
-                  <option value="Fee">Family</option>
-                  <option value="Fee">Electronic</option>
-                  <option value="Fee">Recharge</option>
-                  <option value="Fee">Ration or Rent</option>
-                </select>
-              </div>
-              <div className="date">
-                <input
-                  type="date"
-                  name=""
-                  className="w-full sm:w-3/5 p-4 text-2xl rounded-lg mt-5 text-black"
-                />
-              </div>
-              <div className="submit-expense">
-                <button className="bg-[#FDF0D1] text-red-600 text-2xl p-3 rounded-2xl mt-5 w-full sm:w-3/5">
-                  Submit
-                </button>
-              </div>
-            </h1>
+              <input
+                type="text"
+                ref={inputRef}
+                value={35000}
+                className="salary w-28 bg-[#AC7D88] text-red-900 font-bold text-2xl"
+                disabled
+              />
+            </h3>
+            <CiEdit className="bg-[#643843] rounded-lg cursor-pointer text-2xl mr-4 text-white"/>
+            
+            <h3 className="budget text-2xl font-bold text-[#FDF0D1]">
+              Balance :{" "}
+              <input
+                type="text"
+                value={20000}
+                className="balace w-28 bg-[#AC7D88] text-red-900 font-bold text-2xl"
+                disabled
+              />
+            </h3>
+          </div>
+          <div className="adding-area mb-10 ml-5 mr-5">
+            <div className="add-amt">
+              <input
+                type="text"
+                placeholder="Amount here."
+                className="w-full sm:w-3/5 p-4 bg-[#FDF0D1] text-[#AC7D88] text-2xl placeholder-gray-400 rounded-lg border-2 border-red-700"
+              />
+            </div>
+            <div className="task  text-black">
+              <select
+                name=""
+                id=""
+                className="rounded-lg w-full sm:w-3/5 mt-5 text-2xl p-4 border-2 border-red-700"
+              >
+                <option value="Fee">College Fee</option>
+                <option value="Fee">Personal</option>
+                <option value="Fee">Family</option>
+                <option value="Fee">Electronic</option>
+                <option value="Fee">Recharge</option>
+                <option value="Fee">Ration or Rent</option>
+              </select>
+            </div>
+            <div className="date">
+              <input
+                type="date"
+                name=""
+                className="w-full sm:w-3/5 p-4 text-2xl rounded-lg mt-5 text-black border-2 border-red-700"
+              />
+            </div>
+            <div className="submit-expense">
+              <button className="bg-[#FDF0D1] text-red-600 text-2xl p-3 rounded-2xl mt-5 w-full sm:w-3/5 border-2 border-red-700 hover:bg-red-200">
+                Submit
+              </button>
+            </div>
           </div>
         </div>
         <div className="right">
@@ -101,7 +125,7 @@ function App() {
         </div>
       </div>
 
-      <div className="chart">
+      {/* <div className="chart">
         <PieChart
           data={[
             { title: "One", value: 10, color: "#E38627" },
@@ -113,7 +137,7 @@ function App() {
           height={200}
         />
         ;
-      </div>
+      </div> */}
     </>
   );
 }
