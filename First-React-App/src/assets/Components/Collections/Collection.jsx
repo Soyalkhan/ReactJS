dcfc3b15f31d47b12c14dee6c09855597f1c1ad8
soyalkhan <soyalkhan76@gmail.com>
2024-03-30 17:08:17 +0530

    change-project

--- First-React-App/src/assets/Components/Collections/Collection.jsx
+++ First-React-App/src/assets/Components/Collections/Collection.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Collection() {
   return (
-    <div>Collection</div>
+    <>
+    <div className="main-collection">
+      <div className="mens">
+        <div className="cat-1">
+          <img src="" alt="" />
+        </div>
+        <div className="cat-2"></div>
+        <div className="cat-3"></div>
+        <div className="cat-4"></div>
+      </div>
+      <div className="womens">
+      <div className="cat-1"></div>
+        <div className="cat-2"></div>
+        <div className="cat-3"></div>
+        <div className="cat-4"></div>
+      </div>
+    </div>
+    </>
   )
 }
 
